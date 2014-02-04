e69bd5ac74cf2806873394d6287440bd9bed30ec
Dennis Roberts <dennis@iplantcollaborative.org>
2014-02-04 16:59:52 -0700

    CORE-5133: added some fields that are required by Sonatype to project.clj

--- project.clj
+++ project.clj
@@ -1,10 +1,18 @@
-(defproject clj-icat-direct "0.0.1"
+(defproject org.iplantc/clj-icat-direct "0.0.2-SNAPSHOT"
   :description "A Clojure library for accessing the iRODS ICAT database directly."
   :url "http://github.com/iPlantCollaborativeOpenSource/clj-icat-direct/"
-  :license {:name "BSD"
-            :url "http://github.com/iPlantCollaborativeOpenSource/clj-icat-direct/blob/master/LICENSE"}
+  :license {:name "BSD Standard License"
+            :url "http://www.iplantcollaborative.org/sites/default/files/iPLANT-LICENSE.txt"}
+  :scm {:connection "scm:git:git@github.com:iPlantCollaborativeOpenSource/kameleon.git"
+        :developerConnection "scm:git:git@github.com:iPlantCollaborativeOpenSource/kameleon.git"
+        :url "git@github.com:iPlantCollaborativeOpenSource/kameleon.git"}
+  :pom-addition [:developers
+                 [:developer
+                  [:url "https://github.com/orgs/iPlantCollaborativeOpenSource/teams/iplant-devs"]]]
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [korma "0.3.0-RC5"]
                  [org.postgresql/postgresql "9.2-1002-jdbc4"]]
-  :repositories {"iplantCollaborative"
-                  "http://projects.iplantcollaborative.org/archiva/repository/internal/"})
+  :repositories [["sonatype-nexus-snapshots"
+                  {:url "https://oss.sonatype.org/content/repositories/snapshots"}]
+                 ["sonatype-nexus-staging"
+                  {:url "https://oss.sonatype.org/service/local/staging/deploy/maven2/"}]])
